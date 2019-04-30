244223059fc299e7795f2bb9d53cdd04f2a3f6e2
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-04-30 14:22:57 +0200

    Merge challenge completed successfully.

--- working_with_pdf/merged_challenge.py
+++ working_with_pdf/merged_challenge.py
@@ -1,32 +1,27 @@
-'''From: https://realpython.com/pdf-python/'
+'''From: https://realpython.com/pdf-python/ Continuation of merge.py.
 One item I would like to point out is that you could enhance this script a bit by adding in a range of pages to be added
 if you didn’t want to merge all the pages of each PDF.
  If you’d like a challenge, you could also create a command line interface for this
  function using Python’s argparse module.'''
+
 import argparse
+from typing import List, Optional
+
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--number", help="Number of pdf pages you want to merge. "
-                                           "Divide files with ','. If all pages: write 'd'.")
-args = parser.parse_args()
-numbers = args.number.split(',')
 
-
-def merge_pdfs(paths, output):
+def merge_pdfs(paths, num_pages: Optional[List], output='d.pdf'):
     pdf_writer = PdfFileWriter()
+    if num_pages is None:
+        num_pages = [PdfFileReader(path).getNumPages() for path in paths]
 
     for path in paths:
         pdf_reader = PdfFileReader(path)
-        if args.number:
-            i = paths.index(path)
-            if numbers[i] !='d':
-                for page in range(int(numbers[i])):
-                    pdf_writer.addPage(pdf_reader.getPage(page))
-            else:
-                for page in range(pdf_reader.getNumPages()):
-                    pdf_writer.addPage(pdf_reader.getPage(page))
 
+        i = paths.index(path)
+        if num_pages[i] != 'a':
+            for page in range(int(num_pages[i])):
+                pdf_writer.addPage(pdf_reader.getPage(page))
         else:
             for page in range(pdf_reader.getNumPages()):
                 pdf_writer.addPage(pdf_reader.getPage(page))
@@ -35,6 +30,22 @@ def merge_pdfs(paths, output):
         pdf_writer.write(out)
 
 
+def command_line(paths):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", nargs=len(paths), help="Number of first n pdf pages you want to merge. "
+                                                                 "If all pages: write 'a'.")
+    args = parser.parse_args()
+
+    if args.number:
+        num_pages = args.number
+    else:
+        num_pages = None
+
+    result = merge_pdfs(paths, num_pages, output='d.pdf')
+    return result
+
+
 if __name__ == '__main__':
     paths = ['reportlab-sample.pdf', 'rotated_pages.pdf']
-    merge_pdfs(paths, output='d.pdf')
+
+    command_line(paths)
